66083770bd153da4ac72bd6fa0eb342a0d16e332
davidkecskemeti <dxdy421@gmail.com>
2021-07-23 18:26:41 +0100

    coin collection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Transform groundCheckTransform=null;
+    [SerializeField] LayerMask playerMask;
+    bool jumpKeyWasPressed;
+    float horizontalInput;
+    Rigidbody rigidbodyComponent;
+    int superJumpsRemaining=0; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,7 +22,48 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Space Key were pressed");
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f,playerMask).Length==0)
+        {
+            return;
+        }
+
+        //if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length==1)
+        //{
+        //    return;
+        //}
+
+        if ( jumpKeyWasPressed)
+        {
+            float jumpPower = 5f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject); 
+            superJumpsRemaining++;
         }
     }
 
